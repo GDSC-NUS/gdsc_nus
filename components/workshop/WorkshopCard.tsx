96b51b79c603211220b63e2f74cfbe1fa99a25db
Josephaedan <josephaedan@gmail.com>
2022-07-17 09:24:40 +0800

    Added description slot

--- components/workshop/WorkshopCard.tsx
+++ components/workshop/WorkshopCard.tsx
@@ -11,6 +11,7 @@ export type WorkshopCard = {
   image: JSX.Element;
   popupSpeakers: JSX.Element;
   popupImage: JSX.Element;
+  popupDescription: JSX.Element;
   preview?: boolean;
 };
 
@@ -25,6 +26,7 @@ export default function WorkshopCard({
   image,
   popupSpeakers,
   popupImage,
+  popupDescription,
   preview,
 }: WorkshopCard): WorkshopCardInterface {
   const cardTags = useMemo(() => {
@@ -46,7 +48,7 @@ export default function WorkshopCard({
         <div className="relative m-0 flex h-[120px] w-full flex-col space-y-1 rounded-lg bg-cover px-3 pt-1 pb-3">
           <h3 className="w-full text-sm font-bold md:text-xl">{title}</h3>
           <h5 className="w-full text-sm font-medium">{date}</h5>
-          <p className="h-[2rem] flex-shrink overflow-hidden text-ellipsis whitespace-normal text-xs">
+          <p className="h-[2rem] w-full flex-shrink overflow-hidden text-ellipsis whitespace-normal text-start text-xs">
             {excerpt}
           </p>
           <div className="flex-grow"></div>
@@ -72,7 +74,7 @@ export default function WorkshopCard({
           {popupImage}
         </div>
         <div className="flex h-full w-full flex-col space-y-5 lg:w-1/2">
-          <h3 className="text-start text-sm font-bold md:text-xl">{title}</h3>
+          <h3 className="w-full text-start text-[20px] font-bold">{title}</h3>
           <h5 className="w-full text-sm font-medium">{date}</h5>
           <div className="flex w-full space-x-2">
             {cardTags.map((tag) => (
@@ -85,10 +87,7 @@ export default function WorkshopCard({
             ))}
           </div>
           <div className="flex w-full flex-row space-x-2">{popupSpeakers}</div>
-          <p className="flex-shrink overflow-hidden text-ellipsis whitespace-normal text-xs">
-            {excerpt}
-          </p>
-          <div className="flex-grow"></div>
+          <div className="w-full flex-grow">{popupDescription}</div>
         </div>
       </div>
     );
