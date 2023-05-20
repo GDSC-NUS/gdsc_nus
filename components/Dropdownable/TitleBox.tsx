781fe9f065e135c50ab02bce5f06f285c91e1ea8
Sun Ruiheng <sunruiheng2001@gmail.com>
2023-05-20 22:20:26 +0800

    Added TitleBox?

---
+++ components/Dropdownable/TitleBox.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import clsx from "clsx";
+import { useState } from "react";
+import { ReactNode } from "react";
+import { classicNameResolver } from "typescript";
+
+interface TitleBoxProps {
+  title: JSX.Element;
+  className?: string;
+  children?: JSX.Element;
+}
+export default function TitleBox({
+  title,
+  className,
+  children,
+}: TitleBoxProps) {
+  const [expanded, setExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={toggleExpanded}
+        className={clsx(
+          "border-stone-950 w-1/2 flex-row bg-blue-300 p-2 font-bold text-gray-900 hover:bg-blue-200",
+          className
+        )}
+      >
+        {title}
+      </button>
+      {expanded && <div>{children}</div>}
+    </div>
+  );
+}
